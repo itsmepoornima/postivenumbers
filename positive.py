afbea03fe291840d206ca676145762d1aa9d8d03
Poornima Babu <86886698+itsmepoornima@users.noreply.github.com>
2021-10-13 15:54:19 +0530

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,18 @@
+# Python program to all print positive Numbers in a range
+  
+list1 = [12,-7,5,64,-14]
+  
+for num in list1:
+    
+    if num >= 0:
+      
+        print(num,end="\t")
+
+        
+
+list2=[12,14,-95,3]
+for i in list2:
+    if i>=0:
+        print(i)
+
+
